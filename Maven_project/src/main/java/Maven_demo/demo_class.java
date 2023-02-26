ad659af89bd39a97b1d4b7c2e44dfa2fc45a9471
tejalbkale <125913504+tejalbkale@users.noreply.github.com>
2023-02-26 19:30:08 +0530

    Add files via upload

---
+++ Maven_project/src/main/java/Maven_demo/demo_class.java
@@ -0,0 +1,7 @@
+package Maven_demo;
+
+public class demo_class {
+	public String SampleMethod() {
+		return "SampleMethod";
+	}
+}
---
+++ Maven_project/src/test/java/Maven_demo/SampleMethodTest.java
@@ -0,0 +1,14 @@
+package Maven_demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleMethodTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
